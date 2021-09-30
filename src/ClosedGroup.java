b3ee7b998ee7b12792d9eae611daa3e2c2236ff0
drpap96 <62452226+drpap96@users.noreply.github.com>
2021-09-30 22:39:30 +0300

    Update ClosedGroup.java

--- src/ClosedGroup.java
+++ src/ClosedGroup.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class ClosedGroup extends General_Groups {
-	 //τα μέλη της κλειστής ομάδας.
+	
 
 	public ClosedGroup(String name, String description) {
 		nameGroup=name;
